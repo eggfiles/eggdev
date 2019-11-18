89deef97a8ef120097cb6df2f0e8ca34df98b554
eggfiles <petra.eggers@optoprecision.de>
2019-11-18 09:58:47 +0100

    Create controller.cpp
    adding missing file

---
+++ CppQmlCompareBindings/controller.cpp
@@ -0,0 +1,17 @@
+#include "controller.h"
+
+Controller::Controller()
+{
+}
+
+Controller::~Controller()
+{
+
+}
+
+
+void Controller::setDistance(int _distance)
+{
+    m_distance = _distance;
+    emit distanceChanged(m_distance);
+}
